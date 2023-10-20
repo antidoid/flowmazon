6ce2de63eb60d386d4471943010d4b20d5a0e499
antidoid <singhpiyush998@gmail.com>
2023-10-20 20:12:39 +0530

    Fix: Update cart items through cart

--- src/app/cart/actions.ts
+++ src/app/cart/actions.ts
@@ -10,13 +10,27 @@ export async function setProductQuantity(productId: string, quantity: number) {
 
     if (articleInCart) {
         if (quantity == 0) {
-            await prisma.cartItem.delete({
-                where: { id: articleInCart.id }
+            // Updating the cart here while deleting/updating a cart item
+            // helps us to delete outdated carts which are not linked with a user
+            await prisma.cart.update({
+                where: { id: cart.id },
+                data: {
+                    items: {
+                        delete: { id: articleInCart.id }
+                    }
+                }
             })
         } else {
-            await prisma.cartItem.update({
-                where: { id: articleInCart.id },
-                data: { quantity }
+            await prisma.cart.update({
+                where: { id: cart.id },
+                data: {
+                    items: {
+                        update: {
+                            where: { id: articleInCart.id },
+                            data: { quantity }
+                        }
+                    }
+                }
             })
         }
     }
